f1957c87a7c2f0a6aba6fca98595dd3ba114443f
entrpyc <contact@a-angelov.eu>
2020-12-01 10:38:37 +0200

    updated onboarding to be modal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import "./styles/Global.scss"
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Onboarding from './views/Onboarding'
 
 function App() {
+  const [newUser, setNewUser] = useState(!localStorage.getItem('onboarding-completed'))
+  const [modalControl, setModalControl] = useState("onboarding-modal")
+
+  const completeOnboarding = () => {
+    localStorage.setItem('onboarding-completed', '1')
+    setModalControl(modalControl + ' fade-out')
+
+    setTimeout(() => {
+      setNewUser(false)
+    }, 300);
+  }
+
   return (
     <div className="simulate-mobile">
-      <div className="onboarding-modal">
-        <Onboarding />
-      </div>
+      {newUser &&
+        <div className={modalControl}>
+          <Onboarding completeOnboarding={completeOnboarding} />
+        </div>
+      }
       <Router>
         <Switch>
         </Switch>
