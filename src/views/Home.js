65bd6477915b7dfd9bb07966b97e3f7f950421e4
entrpyc <contact@a-angelov.eu>
2020-12-01 14:45:15 +0200

    added header

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
+import "./Home.scss"
+
 function Home() {
   return (
     <section className="home">
       <div className="container">
-        home
+        <header className="flex jc-sb ai-center">
+          <h1><img src="images/boson-logo-nav.png" alt="Boson Protocol Logo"/></h1>
+          <nav className="flex ai-center">
+            <div className="search flex center" role="button"><img src="images/search-icon.svg" alt="Search"/> <p>Search</p></div>
+            <div className="qr-icon" role="button"><img src="images/qr-icon.svg" alt="Scan QR"/></div>
+          </nav>
+        </header>
       </div>
     </section>
   )
