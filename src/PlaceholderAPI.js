a993f1c3cca38e6df8dd7b6a21133b536a560336
entrpyc <contact@a-angelov.eu>
2020-12-02 09:14:50 +0200

    added homeproducts placeholder api

--- src/PlaceholderAPI.js
+++ src/PlaceholderAPI.js
@@ -1,25 +1,25 @@
 export const productBlocks = [
   {
     title: 'Nike Air',
-    image: 'images/product-block-image-temp.png',
+    image: 'images/temp/product-block-image-temp.png',
     price: 0.3,
     deposit: 0.02,
   },
   {
     title: 'Oculus Quest',
-    image: 'images/product-block-image-temp-2.png',
+    image: 'images/temp/product-block-image-temp-2.png',
     price: 0.3,
     deposit: 0.02,
   },
   {
     title: 'Nike Air',
-    image: 'images/product-block-image-temp.png',
+    image: 'images/temp/product-block-image-temp.png',
     price: 0.3,
     deposit: 0.02,
   },
   {
     title: 'Oculus Quest',
-    image: 'images/product-block-image-temp-2.png',
+    image: 'images/temp/product-block-image-temp-2.png',
     price: 0.3,
     deposit: 0.02,
   }
@@ -28,12 +28,12 @@ export const productBlocks = [
 export const cardBlocks = [
   {
     category: 'Cell Phones & Smartphones',
-    image: 'images/card-block-image-temp.png',
+    image: 'images/temp/card-block-image-temp.png',
     background: '#7DB4EE',
   },
   {
     category: 'Cell Phones & Smartphones',
-    image: 'images/card-block-image-temp.png',
+    image: 'images/temp/card-block-image-temp.png',
     background: '#9C96AF',
   },
 ]
@@ -44,4 +44,31 @@ export const categoryMenu = [
   'Shoes',
   'Health & Beauty- internal link',
   'Electronics',
+]
+
+export const homeProducts = [
+  {
+    title: 'XBox One S',
+    image: 'images/temp/home-product-temp-1.png',
+    price: 0.2,
+    deposit: 0.02,
+  },
+  {
+    title: 'Macbook Pro 13” 2019',
+    image: 'images/temp/home-product-temp-2.png',
+    price: 0.4,
+    deposit: 0.02,
+  },
+  {
+    title: 'Nike Air',
+    image: 'images/temp/home-product-temp-3.png',
+    price: 0.3,
+    deposit: 0.02,
+  },
+  {
+    title: 'Playstation 2 package..',
+    image: 'images/temp/home-product-temp-4.png',
+    price: 0.4,
+    deposit: 0.02,
+  },
 ]
