d3bae49fbd7ea2a227a58fa332f0cf89385cffbd
Vladislav Ivanov <ivanov.vladdy@gmail.com>
2020-12-14 10:51:23 +0200

    Console log scanned qr value

--- src/components/QRCodeScanner.js
+++ src/components/QRCodeScanner.js
@@ -16,7 +16,7 @@ function QRCodeScanner() {
 
     const handleScan = (data) => {
         if (data) {
-            // console.log(data);
+            console.log(data);
             //ToDo: Should validate the result
             // const { result, error } = validate(data);
             stopRecording();
