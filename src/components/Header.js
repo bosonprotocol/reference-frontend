7942e6f25ad6a9fc4e8b29e536a6c2b77f0581d3
entrpyc <contact@a-angelov.eu>
2020-12-17 15:47:10 +0200

    added connect button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 import "./Header.scss"
 import { useStore } from "../hooks";
 
@@ -15,10 +17,12 @@ function Header() {
         <header className="flex jc-sb ai-center">
             <h1><img src="images/boson-logo-nav.png" alt="Boson Protocol Logo"/></h1>
             <nav className="flex ai-center">
+                <Link to="/connect">
+                    <div className="button primary" role="button">Connect</div> 
+                </Link>
                 <div className="search flex center" role="button"><img src="images/search-icon.svg" alt="Search"/>
                     <p>Search</p></div>
-                <div className="qr-icon" role="button" onClick={ activateQrReader }><img src="images/qr-icon.svg"
-                                                                                         alt="Scan QR"/></div>
+                <div className="qr-icon" role="button" onClick={ activateQrReader }><img src="images/qr-icon.svg" alt="Scan QR"/></div>
             </nav>
         </header>
     )
