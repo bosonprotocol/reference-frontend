4f8166417cc990af36592442b450a610810421c4
entrpyc <contact@a-angelov.eu>
2021-01-08 12:37:02 +0200

    prepare to merge with develop

--- src/helpers/Dictionary.js
+++ src/helpers/Dictionary.js
@@ -1,50 +1,55 @@
 export const DIC = {
-  COMMITED: 'commited',
-  CONNECTED: 'connected',
-  OPEN_PRODUCT: 'open_product',
-  CLOSE_PRODUCT: 'close_product',
-  ACTIVATE_QR_READER: 'activate_qr_reader',
-  SET_OFFERING_PROGRESS: 'set_offering_progress',
-  GET_OFFERING_PROGRESS: 'get_offering_progress',
-  UPDATE_OFFERING_DATA: 'update_offering_data',
-  LOAD_OFFERING_BACKUP: 'load_offering_backup',
-  RESET_OFFERING_DATA: 'reset_offering_data',
-  NAV: {
-    CONTROL: 'navigationControl',
-    DEF: 'default_view',
-    COMMIT: 'commit_to_buy',
-    REDEEM: 'redeem',
-  },
+    COMMITED: 'commited',
+    CONNECTED: 'connected',
+    OPEN_PRODUCT: 'open_product',
+    CLOSE_PRODUCT: 'close_product',
+    ACTIVATE_QR_READER: 'activate_qr_reader',
+    SET_OFFERING_PROGRESS: 'set_offering_progress',
+    GET_OFFERING_PROGRESS: 'get_offering_progress',
+    UPDATE_OFFERING_DATA: 'update_offering_data',
+    LOAD_OFFERING_BACKUP: 'load_offering_backup',
+    RESET_OFFERING_DATA: 'reset_offering_data',
+    NAV: {
+        CONTROL: 'navigationControl',
+        DEF: 'default_view',
+        COMMIT: 'commit_to_buy',
+        REDEEM: 'redeem',
+    },
+    SHOW_MODAL: 'show_modal'
 }
 
 export const ROUTE = {
-  Home: '/',
-  Connect: '/connect',
-  ConnectToMetamask: '/connect-to-metamask',
-  ShowQR: '/show-qr-code',
-  NewOffer: '/new-offer',
+    Home: '/',
+    Connect: '/connect',
+    ConnectToMetamask: '/connect-to-metamask',
+    ShowQR: '/show-qr-code',
+    NewOffer: '/new-offer',
 }
 
 export const NAME = {
-  CATEGORY: 'category',
-  IMAGE: 'image',
-  TITLE: 'title',
-  QUANTITY: 'quantity',
-  CONDITION: 'condition',
-  DESCRIPTION: 'description',
-  PRICE_C: 'price_currency',
-  PRICE: 'price',
-  PRICE_SUFFIX: 'price_suffix',
-  SELLER_DEPOSIT_C: 'seller_deposit_currency',
-  SELLER_DEPOSIT: 'seller_deposit',
-  SELLER_SUFFIX: 'seller_suffix',
-  BUYER_DEPOSIT: 'buyer_deposit',
-  BUYER_SUFFIX: 'buyer_suffix',
-  DATE_START: 'start_date',
-  DATE_END: 'end_date'
+    CATEGORY: 'category',
+    IMAGE: 'image',
+    TITLE: 'title',
+    QUANTITY: 'quantity',
+    CONDITION: 'condition',
+    DESCRIPTION: 'description',
+    PRICE_C: 'price_currency',
+    PRICE: 'price',
+    PRICE_SUFFIX: 'price_suffix',
+    SELLER_DEPOSIT_C: 'seller_deposit_currency',
+    SELLER_DEPOSIT: 'seller_deposit',
+    SELLER_SUFFIX: 'seller_suffix',
+    BUYER_DEPOSIT: 'buyer_deposit',
+    BUYER_SUFFIX: 'buyer_suffix',
+    DATE_START: 'start_date',
+    DATE_END: 'end_date'
 }
 
+export const MODAL_TYPES = {
+    GENERIC_ERROR: 'generic_error'
+};
+
 export const CURRENCY = {
-  ETH: 'ETH',
-  BSN: 'BSN',
+    ETH: 'ETH',
+    BSN: 'BSN',
 }
