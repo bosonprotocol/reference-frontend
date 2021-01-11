c5d136b2300b41905f75ae2778f0f9defaf77b67
entrpyc <contact@a-angelov.eu>
2021-01-11 08:43:40 +0200

    added activity structure

--- src/views/Activity.js
+++ src/views/Activity.js
@@ -1,11 +1,39 @@
 import React from 'react'
 
 function Activity() {
-  const a = [<li>a</li>, <li>b</li>, <li>c</li>]
+  const status = 'OFFERED';
+  const date = 'Oct 26th 2020';
+  const title = 'Nike Adapt Self-Lacing Smart Sneaker';
+  const value = 0.1;
+  const currency = 'ETH';
+  const quantity = 1;
+  const thumbnail = 'images/temp/product-block-image-temp-2.png';
 
   return (
-    a
+    <section className="activity">
+      <div className="container">
+        <div className="vouchers-container">
+          <div className="voucher-block flex">
+            <div className="thumb no-shrink">
+              <img src={thumbnail} alt={title}/>
+            </div>
+            <div className="info w100">
+              <div className="status flex split">
+                <div className="label">{status}</div>
+                <div className="date">{date}</div>
+              </div>
+              <div className="title">{title}</div>
+              <div className="price flex split">
+                <div className="value">{value} {currency}</div>
+                <div className="quantity">QTY: {quantity}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
 export default Activity
+ 
