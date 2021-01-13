1cc54c615dd4585f85b0d9a9a517b49572adfc0e
entrpyc <contact@a-angelov.eu>
2021-01-13 11:09:06 +0200

    select is disabled

--- src/components/offerFlow/Currencies.js
+++ src/components/offerFlow/Currencies.js
@@ -5,12 +5,12 @@ import { CURRENCY } from "../../helpers/Dictionary"
 function Currencies(props) {
   const { name } = props
   return (
-    <span className="currencies-container">
+    <span className="currencies-container select-disabled">
       <span className="icons">
         <div className={CURRENCY.ETH}></div>
         <div className={CURRENCY.BSN}></div>
       </span>
-      <select className="currencies" name={name} id={name} >
+      <select disabled className="currencies" name={name} id={name} >
         <option value={CURRENCY.ETH}>{CURRENCY.ETH}</option>
         <option value={CURRENCY.BSN}>{CURRENCY.BSN}</option>
       </select>
