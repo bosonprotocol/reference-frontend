e5a6b52d2a54966d97439456df4daabd83f081a1
entrpyc <contact@a-angelov.eu>
2021-01-20 13:22:05 +0200

    added mock data

--- src/components/redemptionFlow/EscrowDiagram.js
+++ src/components/redemptionFlow/EscrowDiagram.js
@@ -3,9 +3,24 @@ import React from 'react'
 import "./EscrowDiagram.scss"
 
 function EscrowDiagram() {
-  // const escrowData = [
-  //   'PAYMENT': 
-  // ]
+  
+  const escrowData = {
+    PAYMENT: {
+      title: 'PAYMENT',
+      value: '0.1 ETH',
+      position: 2,
+    },
+    BUYER_DEPOSIT: {
+      title: 'BUYER DEPOSIT',
+      value: '0.02 ETH',
+      position: 2,
+    },
+    SELLER_DEPOSIT: {
+      title: 'SELLER DEPOSIT',
+      value: '0.01 ETH',
+      position: 2,
+    },
+  }
 
   return (
     <section className="escrow-diagram vertical">
@@ -17,7 +32,7 @@ function EscrowDiagram() {
             <div className="cell">SELLER</div>
           </div>
           <div className="body">
-            <EscrowRow title='aaa' value='vvv' />
+            {Object.values(escrowData).map(row => <EscrowRow { ...row } />)}
           </div>
         </div>
       </div>
@@ -26,13 +41,13 @@ function EscrowDiagram() {
 }
 
 const EscrowRow = (props) => {
-  const { title, value } = props
+  const { title, value, position } = props
 
   return (
     <div className="body-row flex ai-center">
       <div className="cell title">{title}</div>
       <div className="cell flex relative">
-        <div className="val">{value}</div>
+        <div className={`val position _${position}`}>{value}</div>
         <div className="box"></div>
         <div className="box"></div>
         <div className="box"></div>
