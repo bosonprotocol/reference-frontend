fd2cd9ea5a1ed739779cc15c3601acad39da732e
entrpyc <contact@a-angelov.eu>
2021-02-09 12:53:08 +0200

    fixed transparency issue

--- src/helpers/CSS.js
+++ src/helpers/CSS.js
@@ -1,5 +1,5 @@
-export const bgColorPrimary = '#000000'
-export const bgColorSecondary = '#161B21'
+export const bgColorPrimary = '0, 0, 0'
+export const bgColorSecondary = '22, 27, 40'
 export const cssBackgroundColor = '--page-color'
 export const updateCssVar = (prop, value) => document.documentElement.style.setProperty(prop, value)
 export const updateBackgroundColor = (value) => document.documentElement.style.setProperty(cssBackgroundColor, value)
