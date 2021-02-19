45881e071bea45e08c8763c8b96555961e142291
wideyedwonderer <codisanko@gmail.com>
2021-02-19 11:38:50 +0200

    finalized contract versions

--- src/hooks/configs.js
+++ src/hooks/configs.js
@@ -1,7 +1,7 @@
 export const SMART_CONTRACTS = {
-    CashierContractAddress: "0xfFCCfC789bCB40f710395d9d7D79A30872890719",
-    VoucherKernelContractAddress: "0xc83898EcB7D4295B34f11E2048aC0BDD9E46A610",
-    BosonRouterContractAddress: "0x32479F86c5B64d44655419b6AF787e46A5F89b4a"
+    CashierContractAddress: "0x23744592d099fA701C7885a37F5EC0a0288f608d",
+    VoucherKernelContractAddress: "0xCd6818d44D439F3FAcE06cEE87e67D82d929391B",
+    BosonRouterContractAddress: "0x4F412F601Ae23f99Dc310418CC98996F798a3FcC"
 };
 
 export const SMART_CONTRACTS_EVENTS = {
