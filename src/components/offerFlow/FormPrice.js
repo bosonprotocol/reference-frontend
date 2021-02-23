02b0205f18f29f1a05f85e0db72699d9d8ced57d
wideyedwonderer <codisanko@gmail.com>
2021-02-23 10:52:42 +0200

    formating

--- src/components/offerFlow/FormPrice.js
+++ src/components/offerFlow/FormPrice.js
@@ -10,7 +10,7 @@ import { toFixed } from '../../utils/format-utils'
 
 
 function FormPrice({
-  depositsPriceLimits, 
+  depositsPriceLimits,
   priceValueReceiver,
   priceCurrencyReceiver,
   sellerDepositCurrencyValueReceiver,
@@ -27,7 +27,7 @@ function FormPrice({
   const getOfferingData = getData(sellerContext.state.offeringData)
 
   const quantity = getOfferingData(NAME.QUANTITY);
-  
+
   const priceCurrency = getOfferingData(NAME.PRICE_C) || 'ETH'
   const sellerCurrency = getOfferingData(NAME.SELLER_DEPOSIT_C) || 'ETH'
   const buyer = getOfferingData(NAME.BUYER_DEPOSIT) || 'ETH'
@@ -36,28 +36,27 @@ function FormPrice({
   const buyerDeposit = getOfferingData(NAME.BUYER_DEPOSIT);
 
   const calculateMaxForCurrency = (currency) => {
-    if(currency) {
+    if (currency) {
       const maxFromContract = depositsPriceLimits[currency].max;
-      if(quantity) {
+      if (quantity) {
         const maxWithQuantityTakenIntoAccount = maxFromContract.div(quantity);
         return toFixed(+ethers.utils.formatEther(maxWithQuantityTakenIntoAccount), 2)
       }
       return ethers.utils.formatEther(maxFromContract)
     }
 
-
   }
   const isValid = (value) => {
     try {
       ethers.utils.parseEther(value);
       return true
-    }catch(e) {
+    } catch (e) {
       return false;
     }
   }
 
   const updateValueIfValid = (event, valueReceiver) => {
-    if(!event || isNaN(parseInt(event.target.value)) || !isValid(event.target.value)) {
+    if (!event || isNaN(parseInt(event.target.value)) || !isValid(event.target.value)) {
       valueReceiver(null)
       return;
     }
@@ -69,7 +68,7 @@ function FormPrice({
         <div className="field">
           <label htmlFor="offer-quantity">Quantity</label>
           <div className="input focus" data-error={quantityErrorMessage}>
-            <input id="offer-quantity" type="number"  onChange={(e) => quantityValueReceiver(e.target ? e.target.value : null)}/>
+            <input id="offer-quantity" type="number" onChange={(e) => quantityValueReceiver(e.target ? e.target.value : null)} />
           </div>
         </div>
       </div>
@@ -77,25 +76,25 @@ function FormPrice({
         <div className="field dual">
           <label htmlFor="offer-price">Payment Price Per Voucher</label>
           <div className="bind">
-            <Currencies inputValueHandler={priceCurrencyReceiver}/>
+            <Currencies inputValueHandler={priceCurrencyReceiver} />
             <div className="input relative focus" data-error={priceErrorMessage ? "" : null}>
-              <input style={priceErrorMessage ? { color: '#FA5B66'} : {}}
-              id="offer-price" type="number" onChange={(e) => updateValueIfValid(e,priceValueReceiver)}/>
+              <input style={priceErrorMessage ? { color: '#FA5B66' } : {}}
+                id="offer-price" type="number" onChange={(e) => updateValueIfValid(e, priceValueReceiver)} />
               {
-                depositsPriceLimits[priceCurrency]?.max ?              
-                <div className="max">max {depositsPriceLimits[priceCurrency] ? calculateMaxForCurrency(priceCurrency) : null} {priceCurrency}</div>
-                : null
+                depositsPriceLimits[priceCurrency]?.max ?
+                  <div className="max">max {depositsPriceLimits[priceCurrency] ? calculateMaxForCurrency(priceCurrency) : null} {priceCurrency}</div>
+                  : null
               }
             </div>
-           
-            
+
+
           </div>
         </div>
       </div>
       {
-              quantity > 1 && price ? 
-              getLimitCalculationsBar(price, quantity, priceCurrency, priceErrorMessage)
-              : null
+        quantity > 1 && price ?
+          getLimitCalculationsBar(price, quantity, priceCurrency, priceErrorMessage)
+          : null
       }
       <div className="row">
         <div className="field dual">
@@ -103,41 +102,41 @@ function FormPrice({
           <div className="bind">
             <Currencies inputValueHandler={sellerDepositCurrencyValueReceiver} />
             <div className="input relative focus" data-error={sellerDepositErrorMessage ? '' : null}>
-              <input style={sellerDepositErrorMessage ? { color: '#FA5B66'} : {}}
-              id="offer-seller-deposit" type="number" onChange={(e) => updateValueIfValid(e, sellerDepositValueReceiver)} />
+              <input style={sellerDepositErrorMessage ? { color: '#FA5B66' } : {}}
+                id="offer-seller-deposit" type="number" onChange={(e) => updateValueIfValid(e, sellerDepositValueReceiver)} />
               {
-                depositsPriceLimits[sellerCurrency]?.max? 
-                <div className="max">max {depositsPriceLimits[sellerCurrency] ?  calculateMaxForCurrency(sellerCurrency) : null} {sellerCurrency}</div>
-                : null
+                depositsPriceLimits[sellerCurrency]?.max ?
+                  <div className="max">max {depositsPriceLimits[sellerCurrency] ? calculateMaxForCurrency(sellerCurrency) : null} {sellerCurrency}</div>
+                  : null
               }
             </div>
           </div>
         </div>
       </div>
       {
-              quantity > 1 && sellerDeposit ? 
-              getLimitCalculationsBar(sellerDeposit, quantity, sellerCurrency, sellerDepositErrorMessage)
-              : null
+        quantity > 1 && sellerDeposit ?
+          getLimitCalculationsBar(sellerDeposit, quantity, sellerCurrency, sellerDepositErrorMessage)
+          : null
       }
       <div className="row">
         <div className="field">
           <label htmlFor="offer-buyer-deposit">Buyer’s Deposit Per Voucher</label>
           <div className="input relative focus" data-error={buyerDepositErrorMessage ? "" : null}>
             <div name={NAME.PRICE_SUFFIX} className="pseudo">{`${buyer} ${priceCurrency}`}</div>
-            <input id="offer-buyer-deposit" style={buyerDepositErrorMessage ? { color: '#FA5B66'} : {}}
-            type="number" name={NAME.BUYER_DEPOSIT} onChange={(e) => updateValueIfValid(e,buyerDepositValueReceiver)}/>
+            <input id="offer-buyer-deposit" style={buyerDepositErrorMessage ? { color: '#FA5B66' } : {}}
+              type="number" name={NAME.BUYER_DEPOSIT} onChange={(e) => updateValueIfValid(e, buyerDepositValueReceiver)} />
             {
               depositsPriceLimits[priceCurrency].max ?
-              <div className="max">max {depositsPriceLimits[priceCurrency] ? calculateMaxForCurrency(priceCurrency) : null} {priceCurrency}</div> 
-              : null
+                <div className="max">max {depositsPriceLimits[priceCurrency] ? calculateMaxForCurrency(priceCurrency) : null} {priceCurrency}</div>
+                : null
             }
           </div>
         </div>
       </div>
       {
-              quantity > 1 && buyerDeposit ? 
-              getLimitCalculationsBar(buyerDeposit, quantity, sellerCurrency, buyerDepositErrorMessage)
-              : null
+        quantity > 1 && buyerDeposit ?
+          getLimitCalculationsBar(buyerDeposit, quantity, sellerCurrency, buyerDepositErrorMessage)
+          : null
       }
     </div>
   )
@@ -147,16 +146,16 @@ export default FormPrice
 
 const getLimitCalculationsBar = (amount, quantity, currency, errorMessage) => (
 
-  <div className="row flex split" style={ {background: '#151A1F', height: '50px', marginTop: '20px'}}>
-    <p className="flex" style={ {padding: '15px 13px'}}>
-      <span className="field dual" style={errorMessage ? { color: '#FA5B66'} : {}} >
+  <div className="row flex split" style={{ background: '#151A1F', height: '50px', marginTop: '20px' }}>
+    <p className="flex" style={{ padding: '15px 13px' }}>
+      <span className="field dual" style={errorMessage ? { color: '#FA5B66' } : {}} >
         {`${ethers.utils.formatEther(amount)} ${currency} `}
-      </span>  
+      </span>
       <span className="field dual">
-      &nbsp; {` x ${quantity} voucher${quantity > 1 ? 's' : ''}`}
+        &nbsp; {` x ${quantity} voucher${quantity > 1 ? 's' : ''}`}
       </span>
     </p>
-    <p className="field dual" style={ {padding: '15px 13px'}}>
+    <p className="field dual" style={{ padding: '15px 13px' }}>
       {`=    ${ethers.utils.formatEther(amount.mul(quantity))} ${currency}`}
     </p>
   </div>
