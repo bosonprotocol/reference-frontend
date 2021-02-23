ed98e6bc896dafc72125d9365c1dd06825d3a019
wideyedwonderer <codisanko@gmail.com>
2021-02-23 14:00:15 +0200

    made validation errors to be shown only after first blur

--- src/components/offerFlow/FormPrice.js
+++ src/components/offerFlow/FormPrice.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 
 import { SellerContext, getData } from "../../contexts/Seller"
 
@@ -23,6 +23,11 @@ function FormPrice({
   buyerDepositErrorMessage
 }) {
   const sellerContext = useContext(SellerContext)
+  
+  const [quantityHasBeenBlurred, setQuantityHasBeenBlurred] = useState(false);
+  const [priceHasBeenBlurred, setPriceHasBeenBlurred] = useState(false);
+  const [sellerDepositHasBeenBlurred, setSellerDepositHasBeenBlurred] = useState(false);
+  const [buyerDepositHasBeenBlurred, setBuyerDepositHasBeenBlurred] = useState(false);
 
   const getOfferingData = getData(sellerContext.state.offeringData)
 
@@ -35,8 +40,8 @@ function FormPrice({
       <div className="row">
         <div className="field">
           <label htmlFor="offer-quantity">Quantity</label>
-          <div className="input focus" data-error={quantityErrorMessage}>
-            <input id="offer-quantity" type="number"  onChange={(e) => quantityValueReceiver(e.target ? e.target.value : null)}/>
+          <div className="input focus" data-error={quantityHasBeenBlurred ? quantityErrorMessage : null}>
+            <input id="offer-quantity" type="number" onBlur={()=> setQuantityHasBeenBlurred(true)}  onChange={(e) => quantityValueReceiver(e.target ? e.target.value : null)}/>
           </div>
         </div>
       </div>
@@ -45,9 +50,9 @@ function FormPrice({
           <label htmlFor="offer-price">Payment Price</label>
           <div className="bind">
             <Currencies inputValueHandler={priceCurrencyReceiver}/>
-            <div className="input relative focus" data-error={priceErrorMessage}>
+            <div className="input relative focus" data-error={priceHasBeenBlurred ? priceErrorMessage : null}>
               <input
-              id="offer-price" type="number" onChange={(e) => priceValueReceiver(e.target ? e.target.value : null)}/>
+              id="offer-price" type="number" onBlur={()=> setPriceHasBeenBlurred(true)} onChange={(e) => priceValueReceiver(e.target ? e.target.value : null)}/>
               <div className="max">max {priceSettings[priceCurrency] ? priceSettings[priceCurrency].max : null} {priceCurrency}</div>
             </div>
           </div>
@@ -56,10 +61,10 @@ function FormPrice({
       <div className="row">
         <div className="field">
           <label htmlFor="offer-buyer-deposit">Buyer’s Deposit</label>
-          <div className="input relative focus" data-error={buyerDepositErrorMessage}>
+          <div className="input relative focus" data-error={buyerDepositHasBeenBlurred ? buyerDepositErrorMessage : null}>
             <div name={NAME.PRICE_SUFFIX} className="pseudo">{`${buyer} ${priceCurrency}`}</div>
             <input id="offer-buyer-deposit"
-            type="number" name={NAME.BUYER_DEPOSIT} onChange={(e) => buyerDepositValueReceiver(e.target ? e.target.value : null)}/>
+            type="number" name={NAME.BUYER_DEPOSIT} onBlur={()=> setBuyerDepositHasBeenBlurred(true)} onChange={(e) => buyerDepositValueReceiver(e.target ? e.target.value : null)}/>
             <div className="max">max {buyerSettings[priceCurrency] ? buyerSettings[priceCurrency].max : null} {priceCurrency}</div> 
           </div>
         </div>
@@ -69,9 +74,9 @@ function FormPrice({
           <label htmlFor="offer-seller-deposit">Seller’s Deposit</label>
           <div className="bind">
             <Currencies inputValueHandler={sellerDepositCurrencyValueReceiver} />
-            <div className="input relative focus" data-error={sellerDepositErrorMessage}>
+            <div className="input relative focus" data-error={sellerDepositHasBeenBlurred ? sellerDepositErrorMessage: null}>
               <input
-              id="offer-seller-deposit" type="number" onChange={(e) => sellerDepositValueReceiver(e.target ? e.target.value : null)} />
+              id="offer-seller-deposit" type="number" onBlur={()=> setSellerDepositHasBeenBlurred(true)} onChange={(e) => sellerDepositValueReceiver(e.target ? e.target.value : null)} />
               <div className="max">max {sellerSettings[sellerCurrency] ? sellerSettings[sellerCurrency].max : null} {sellerCurrency}</div>
             </div>
           </div>
