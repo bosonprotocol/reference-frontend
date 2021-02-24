f3ecdb0c2690a2cdf01f836939c307a0ae782e0c
wideyedwonderer <codisanko@gmail.com>
2021-02-24 12:26:35 +0200

    route fixed

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3005'
+    baseURL: process.env.REACT_APP_BACKEND_BASE_URL
 });
 
 export const generateNonce = async (address) => {
