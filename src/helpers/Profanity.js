73cfa9bcf8477e4ba6a2ae7076094c93e3f9ed07
entrpyc <contact@a-angelov.eu>
2021-02-24 13:25:52 +0200

    removed log and fixed top navigation showing above success screen

--- src/helpers/Profanity.js
+++ src/helpers/Profanity.js
@@ -870,8 +870,6 @@ export const latinise = (str) =>{return str.replace(/[^A-Za-z\[\] ]/g,function(a
 export const profanityTest = (inputRaw) => {
   // merge the input into single string of letters and numbers only
   let input = '-' + inputRaw.split(/[^a-zA-Z0-9]+/).filter(v=>v).join('-').toLowerCase() + '-'
-  // console.log(inputRaw)
-  console.log(input)
 
   // convert special characters to latin
   input = latinise(input)
