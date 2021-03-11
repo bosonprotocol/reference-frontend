688d06f13f5e287ba381f983ff48827480c1c6dd
entrpyc <contact@a-angelov.eu>
2021-03-11 12:20:03 +0200

    added no voucher sets message

--- src/views/Activity.js
+++ src/views/Activity.js
@@ -24,6 +24,17 @@ import Loading from "../components/offerFlow/Loading";
 import { WalletConnect } from "../components/modals/WalletConnect"
 import { formatDate } from "../helpers/Format"
 
+const MessageType = {
+    [VOUCHER_TYPE.voucherSet]: {
+        active: 'open voucher sets',
+        inactive: 'closed voucher sets',
+    },
+    [VOUCHER_TYPE.accountVoucher]: {
+        active: 'active vouchers',
+        inactive: 'inactive vouchers',
+    }
+}
+
 export function ActivityAccountVouchers({title, voucherSetId, block}) {
     const [accountVouchers, setAccountVouchers] = useState([])
     const { account } = useWeb3React();
@@ -132,10 +143,10 @@ function ActivityView(props) {
         setInactiveVouchers(blocksSorted.inactive?.sort((a, b) => getLastAction(a) > getLastAction(b) ? -1 : 1))
     }, [resultVouchers])
 
-    const activityMessage = (tab) => {
+    const activityMessage = (messageType) => {
         return account ?
         <div className="no-vouchers flex column center">
-        <p>You currently have no {`${tab?'active':'inactive'}`} vouchers.</p>
+        <p>You currently have no {messageType}.</p>
         <IconActivityMessage />
         </div>
         :
@@ -163,13 +174,13 @@ function ActivityView(props) {
                             <TabPanel>
                                 {activeVouchers?.length > 0 && !!account?
                                     <ActiveTab voucherSetId={voucherSetId && voucherSetId} voucherType={voucherType} products={ activeVouchers }/> :
-                                    activityMessage(1)
+                                    activityMessage(MessageType[voucherType].active)
                                 }
                             </TabPanel>
                             <TabPanel>
                                 {inactiveVouchers?.length > 0 && !!account?
                                     <ActiveTab voucherSetId={voucherSetId && voucherSetId} voucherType={voucherType} products={ inactiveVouchers }/> :
-                                    activityMessage()
+                                    activityMessage(MessageType[voucherType].inactive)
                                 }
                             </TabPanel>
                         </> 
