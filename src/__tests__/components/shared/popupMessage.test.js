2ee254b9165671672b63ba561d0aacdab45dedc3
Dennis Furrer <dennis.furrer@gmail.com>
2021-03-22 22:11:49 +0000

    Component Tests: ProductBlock - Added for price/deposit combinations

--- src/__tests__/components/shared/popupMessage.test.js
+++ src/__tests__/components/shared/popupMessage.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
+import { act } from "@testing-library/react";
 
 import PopupMessage from "../../../components/shared/PopupMessage";
 
