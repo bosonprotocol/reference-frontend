909dfac276e5207530f788411d4134175f2e7c77
HristiyanG <hristiyan@limechain.tech>
2021-03-24 17:31:11 +0200

    changed address

--- src/hooks/configs.js
+++ src/hooks/configs.js
@@ -2,7 +2,7 @@ export const SMART_CONTRACTS = {
     CashierContractAddress: "0x804bD23de6337d32b55F6eB711A69F9Ff3929544",
     VoucherKernelContractAddress: "0x4321300118BB6Bb42c1025F84c5FbD0Ba818A932",
     BosonRouterContractAddress: "0x055fe7ad69F3e37bca2Cced75879b975484cDFA9",
-    BosonTokenContractAddress: "0x10DE49a60C74A8C0c988EF83A2DFABF218ca48E3",
+    BosonTokenContractAddress: "0x5c70A0c47440128eAAA66801B0ec04E9d8C3a570",
     FundLimitsContractAddress: "0x50F0c315D460F1ce3708b008A6B7327F8Fd0EA49",
 };
 
