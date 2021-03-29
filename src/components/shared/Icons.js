a2e6ad3757767c1d7b51ee495d4ded573c3d087a
Toby Clemson <tobyclemson@gmail.com>
2021-03-29 14:54:50 +0100

    Eject from create-react-app.

--- src/components/shared/Icons.js
+++ src/components/shared/Icons.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 export const IconCalendar = ({ color, direction }) => (
   direction ?
   <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
