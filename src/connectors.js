c09ea026311ac97e92510a6598cf2785c92f0d58
HristiyanG <hristiyan@limechain.tech>
2021-03-31 09:57:06 +0300

    lint

--- src/connectors.js
+++ src/connectors.js
@@ -4,17 +4,17 @@ import { NetworkConnector } from "./utils/networkConnector";
 
 const POLLING_INTERVAL = 12000;
 const RPC_URLS = {
-    1: "https://mainnet.infura.io/v3/653584f4572b4aa894dfa26281f834f2",
-    4: "https://rinkeby.infura.io/v3/653584f4572b4aa894dfa26281f834f2",
-    5: "https://goerli.infura.io/v3/653584f4572b4aa894dfa26281f834f2",
-    42: "https://kovan.infura.io/v3/653584f4572b4aa894dfa26281f834f2",
+  1: "https://mainnet.infura.io/v3/653584f4572b4aa894dfa26281f834f2",
+  4: "https://rinkeby.infura.io/v3/653584f4572b4aa894dfa26281f834f2",
+  5: "https://goerli.infura.io/v3/653584f4572b4aa894dfa26281f834f2",
+  42: "https://kovan.infura.io/v3/653584f4572b4aa894dfa26281f834f2",
 };
 
 export const NETWORK_ID = 5;
 
 // rinkeby
 export const network = new NetworkConnector({
-    urls: { 5: RPC_URLS[5] },
+  urls: { 5: RPC_URLS[5] },
 });
 
 export const injected = new InjectedConnector({
@@ -23,8 +23,8 @@ export const injected = new InjectedConnector({
 
 // rinkeby
 export const walletconnect = new WalletConnectConnector({
-    rpc: { 5: RPC_URLS[5] },
-    bridge: "https://bridge.walletconnect.org",
-    qrcode: true,
-    pollingInterval: POLLING_INTERVAL,
+  rpc: { 5: RPC_URLS[5] },
+  bridge: "https://bridge.walletconnect.org",
+  qrcode: true,
+  pollingInterval: POLLING_INTERVAL,
 });
--- src/hooks/configs.js
+++ src/hooks/configs.js
@@ -1,9 +1,9 @@
 export const SMART_CONTRACTS = {
-    CashierContractAddress: "0xcdF2dd01f20F0e1844CE23d17895E986dad39f18",
-    VoucherKernelContractAddress: "0xD3EB292E3D3261f836b4de0b8E446d127aa14d64",
-    BosonRouterContractAddress: "0xe23dF284c115daD7028d18Dd502b54Ea30AB1e2e",
-    BosonTokenContractAddress: "0x2Bbba6a961803528700d4B47a586aFA54c2f5D1D",
-    FundLimitsContractAddress: "0xfe5c5aB6764Bf161313434BCa9747F63B629B566",
+  CashierContractAddress: "0xcdF2dd01f20F0e1844CE23d17895E986dad39f18",
+  VoucherKernelContractAddress: "0xD3EB292E3D3261f836b4de0b8E446d127aa14d64",
+  BosonRouterContractAddress: "0xe23dF284c115daD7028d18Dd502b54Ea30AB1e2e",
+  BosonTokenContractAddress: "0x2Bbba6a961803528700d4B47a586aFA54c2f5D1D",
+  FundLimitsContractAddress: "0xfe5c5aB6764Bf161313434BCa9747F63B629B566",
 };
 
 export const SMART_CONTRACTS_EVENTS = {
