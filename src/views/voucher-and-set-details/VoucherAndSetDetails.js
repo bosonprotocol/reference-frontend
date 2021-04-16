3ca0c465230ba0cc30c8e8d8e3be875ccc1f80bd
wideyedwonderer <codisanko@gmail.com>
2021-04-16 17:17:01 +0300

    confirm actions

--- src/views/voucher-and-set-details/VoucherAndSetDetails.js
+++ src/views/voucher-and-set-details/VoucherAndSetDetails.js
@@ -209,7 +209,7 @@ function VoucherAndSetDetails(props) {
   const tableCategory = [["Category", getProp("category")]];
 
   const tableCondition = [["Condition", capitalize(getProp("condition"))]];
-
+  
   const confirmAction = (action, text) => {
     const callAction = () => {
       action();
@@ -282,7 +282,7 @@ function VoucherAndSetDetails(props) {
       <div
         className="action button cof"
         onClick={() =>
-          confirmAction(onCoF, "Are you sure you want to cancel this voucher?")
+          confirmAction(onCoF, "Are you sure you want to cancel/fault?")
         }
         role="button"
       >
@@ -295,7 +295,7 @@ function VoucherAndSetDetails(props) {
         <div
           className="action button refund"
           role="button"
-          onClick={() => onRefund()}
+          onClick={() => confirmAction(onRefund, "Are you sure you want to refund?" )}
         >
           REFUND
         </div>
@@ -318,7 +318,7 @@ function VoucherAndSetDetails(props) {
       <div
         className="action button complain"
         role="button"
-        onClick={() => onComplain()}
+        onClick={() => confirmAction(onComplain(), "Are you sure you want to complain?")}
       >
         COMPLAIN
       </div>
@@ -1486,9 +1486,11 @@ function VoucherAndSetDetails(props) {
                   {getProp("category") ? (
                     <TableRow data={tableCategory} />
                   ) : null}
-                  {getProp("condition") ? (
-                    <TableRow data={tableCondition} />
-                  ) : null}
+                  {
+                    getProp('condition') ? (
+                      <TableRow data={tableCondition}/>
+                     ) : null
+                  }
                 </div>
                 {voucherSetDetails ? (
                   <div className="section price">
