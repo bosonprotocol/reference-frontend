261d2726289c0cac9b33adf928adffa4ee400372
Mohammed Abubakar <mohammed.abubakar@altostack.io>
2021-04-21 14:19:14 +0100

    update deploy command

--- pipelines/develop/provision-content/task.sh
+++ pipelines/develop/provision-content/task.sh
@@ -22,4 +22,4 @@ export AWS_ACCESS_KEY_ID="$(jq -M -r .Credentials.AccessKeyId build/session)"
 export AWS_SECRET_ACCESS_KEY="$(jq -M -r .Credentials.SecretAccessKey build/session)"
 export AWS_SESSION_TOKEN="$(jq -M -r .Credentials.SessionToken build/session)"
 
-./go "app:build[${DEPLOYMENT_TYPE},${DEPLOYMENT_LABEL}]"
+./go "app:deploy[${DEPLOYMENT_TYPE},${DEPLOYMENT_LABEL}]"
