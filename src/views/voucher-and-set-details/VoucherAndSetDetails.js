52aa2cbfca88a89d7410373967dba9656911121a
wideyedwonderer <codisanko@gmail.com>
2021-04-26 09:36:46 +0300

    payment distribution message set

--- src/views/voucher-and-set-details/VoucherAndSetDetails.js
+++ src/views/voucher-and-set-details/VoucherAndSetDetails.js
@@ -149,8 +149,8 @@ function VoucherAndSetDetails(props) {
   const [actionPerformed, setActionPerformed] = useState(1);
   const [popupMessage, setPopupMessage] = useState();
   const [
-    showDepositsDistributionWarningMessage,
-    setShowDepositsDistributionWarningMessage,
+    distributionMessage,
+    setDistributionMessage,
   ] = useState(false);
   const [recentlySignedTxHash, setRecentlySignedTxHash] = useState("");
   const [
@@ -629,6 +629,7 @@ function VoucherAndSetDetails(props) {
     const payments = await getPayments(voucherDetails, account, modalContext);
 
     let depositsDistributed;
+    let paymentDistributed;
 
     if (payments?.distributedAmounts) {
       depositsDistributed =
@@ -636,10 +637,19 @@ function VoucherAndSetDetails(props) {
           ...Object.values(payments?.distributedAmounts.buyerDeposit),
           ...Object.values(payments?.distributedAmounts.sellerDeposit),
         ].filter((x) => x.hex !== "0x00").length > 0;
+
+        paymentDistributed =
+        [
+          ...Object.values(payments?.distributedAmounts.payment),
+        ].filter((x) => x.hex !== "0x00").length > 0;
     }
 
     if (!depositsDistributed && voucherDetails.FINALIZED) {
-      setShowDepositsDistributionWarningMessage(true);
+      setDistributionMessage('Deposits will be distributed in 1 hour');
+    }
+    if(!paymentDistributed && !voucherDetails.FINALIZED && (voucherDetails.REDEEMED || voucherDetails.REFUNDED)) {
+      console.log('setting to')
+      setDistributionMessage('Payment will be distributed in 1 hour');
     }
 
     const getPaymentMatrixSet = (row, column) =>
@@ -1439,10 +1449,10 @@ function VoucherAndSetDetails(props) {
                 </div>
               ) : null}
 
-              {showDepositsDistributionWarningMessage ? (
+              {distributionMessage ? (
                 <div className="section depositsWarning flex center">
                   <IconWarning />{" "}
-                  <span> Deposits will be distributed in 1 hour</span>{" "}
+                  <span> {distributionMessage}</span>{" "}
                 </div>
               ) : null}
 
