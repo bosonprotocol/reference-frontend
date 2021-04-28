ff107876ef4883b4872ad50b506ea5fcb2ae619d
Vlad <ivanov.vladdy@gmail.com>
2021-04-28 16:35:09 +0300

    Skip redundant condition on voucher details view (#310)

--- src/views/voucher-and-set-details/VoucherAndSetDetails.js
+++ src/views/voucher-and-set-details/VoucherAndSetDetails.js
@@ -1199,7 +1199,7 @@ function VoucherAndSetDetails(props) {
   ]);
 
   useEffect(() => {
-    if (!voucherSetDetails && account && globalContext.state.allVoucherSets) {
+    if (!voucherSetDetails && account) {
       initVoucherDetails(
         account,
         modalContext,
