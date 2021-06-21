33eabe8d433dac5d0585964ea4f43a03b7891b1a
Dennis Furrer <dennis.furrer@gmail.com>
2021-06-21 20:06:50 +0100

    Manually extend profanity check to deter poor app use

--- src/utils/Profanity.js
+++ src/utils/Profanity.js
@@ -1,6 +1,24 @@
 const Filter = require("bad-words");
 const filter = new Filter();
 
+filter.addWords(
+    "acid",
+    "alcohol",
+    "amphetamine",
+    "cannabis",
+    "cocaine",
+    "dmt",
+    "drugs",
+    "ecstasy",
+    "heroin",
+    "ketamine",
+    "lsd",
+    "marijuana",
+    "mdma",
+    "meth",
+    "weed"
+);
+
 export const profanityCheck = (input) => {
   let profanityResult = filter.isProfane(input);
 
