857ef0e6a52a16f7b756db2d97513f63660612e3
LuboslavBurdenski <luboslav990226@gmail.com>
2021-08-04 12:32:44 +0300

    removing unnecessary logic

--- src/views/voucher-and-set-details/components/chat/message-list/MessageList.js
+++ src/views/voucher-and-set-details/components/chat/message-list/MessageList.js
@@ -10,10 +10,7 @@ function Chat({ data }) {
     const { account } = useWeb3React();
     const [iconSize, setIconSize] = useState(5);
 
-    const updateDimensions = () => {
-        window.innerWidth <= 768 ? setIconSize(5) : setIconSize(10);
-    }
-
+    const updateDimensions = () => { window.innerWidth <= 768 ? setIconSize(5) : setIconSize(10);}
     const newDataInstance = data.slice(0);
 
     useEffect(() => {
@@ -22,12 +19,6 @@ function Chat({ data }) {
         return () => window.removeEventListener("resize", updateDimensions);
     }, []);
 
-    newDataInstance.forEach((msg, i) => {
-        if (msg.account === account) {
-            newDataInstance[i].account = 'self';
-        }
-    });
-
     return (
         <ol className="chat">
             {
@@ -35,27 +26,21 @@ function Chat({ data }) {
                     const lastMessageDay = newDataInstance[index - 1]?.timestamp.split('T')[0].split('-')[2];
                     const currentMessageDay = message.timestamp.split('T')[0].split('-')[2];
                     const time = message.timestamp.split('T')[1].slice(0, 5);
-
+    
                     return (((newDataInstance[index - 1]?.account !== newDataInstance[index]?.account) || (lastMessageDay === currentMessageDay)) || (index === 0)) ?
-                        <Fragment>
-                            <li key={index} className={message.account}>
-                                
+                            <li key={index} className={message.account == account ? 'self' : 'other'}>
                                     <Identicon address={data[index].account} size={iconSize} />
-                 
                                 <div className="msg">
                                     <p className="address"><span>0x2..24b</ span></p>
                                     <p>{newDataInstance[index]?.message}</p>
                                     <time>{time}</time>
                                 </div>
                             </li>
-                        </ Fragment>
                         :
                         <Fragment>
                             <div className="day">Mon</div>
-                            <li key={index} className={message.account}>
-                               
+                            <li key={index} className={message.account == account ? 'self' : 'other'}>
                                     <Identicon address={data[index].account} size={iconSize} />
-                                
                                 <div className="msg">
                                     <p className="address"><span>0x2..24b</ span></p>
                                     <p>{newDataInstance[index]?.message}</p>
