9c90a019a118c8b67b1ae227f2f94875fd8e04c0
LuboslavBurdenski <luboslav990226@gmail.com>
2021-08-16 12:26:04 +0300

    chat functionality changes

--- src/views/voucher-and-set-details/components/chat/Chat.js
+++ src/views/voucher-and-set-details/components/chat/Chat.js
@@ -7,22 +7,32 @@ import { useWeb3React } from "@web3-react/core";
 import { getThread } from "../../../../hooks/api";
 import { getAccountStoredInLocalStorage } from "../../../../hooks/authenticate";
 import io from "socket.io-client";
+import { useHistory } from "react-router";
 
 const NEW_CHAT_MESSAGE_EVENT = "message";
 const SOCKET_SERVER_URL = "http://localhost:4000";
 
 
 function Chat(voucherDetails) {
+    const history = useHistory();
+    const accountChanged = useRef(false);
     const [text, setText] = useState("");
-    const [roomId, setRoomId] = useState('');
+    const [roomId, setRoomId] = useState("");
     const { account } = useWeb3React();
     const messagesEndRef = useRef();
     const voucherId = Object.values(voucherDetails)[0];
     const socketRef = useRef();
-
     const [messages, setMessages] = useState([]);
     const [thread, setThread] = useState('');
     const authData = getAccountStoredInLocalStorage(account);
+
+    useEffect(() => {
+        if(accountChanged.current){
+            history.push('/');
+        } else  {
+            accountChanged.current = true;
+        }
+    }, [account]);
 
     useEffect(() => {
         messagesEndRef.current?.scrollIntoView({ block: "end", inline: "nearest" });
@@ -30,23 +40,15 @@ function Chat(voucherDetails) {
 
     useEffect(() => {
         setRoomId([voucherId, account].join(","));
-        return (() => {
-            setText('')
-        })
+        disconnect()
+        setText('');
     }, [voucherId, account]);
 
     useEffect(() => {
         console.log('CONNECT')
-
         getSlackThreadData();
         WSConnection();
-        return () => {
-            // Destroys the socket connection 
-            socketRef.current.disconnect(true);
-            // Destroys the socket reference when the connection is closed
-            socketRef.current.off('connection', () => { });
-          
-        };
+        return () => { disconnect() };
     }, [socketRef, thread, voucherId, account]);
 
     const onType = (e) => { setText(e.target.value) };
@@ -69,9 +71,9 @@ function Chat(voucherDetails) {
 
         } else {
 
-             socketRef.current.emit(NEW_CHAT_MESSAGE_EVENT, {
+            socketRef.current.emit(NEW_CHAT_MESSAGE_EVENT, {
                 ...requestData,
-                senderId:  socketRef.current.id,
+                senderId: socketRef.current.id,
             });
 
         }
@@ -99,22 +101,28 @@ function Chat(voucherDetails) {
         }
 
         // Creates a WebSocket connection
-         socketRef.current = io(SOCKET_SERVER_URL, ioParams);
+        socketRef.current = io(SOCKET_SERVER_URL, ioParams);
 
         // Listens for incoming messages
-         socketRef.current.on(NEW_CHAT_MESSAGE_EVENT, (messages) => {
+        socketRef.current.on(NEW_CHAT_MESSAGE_EVENT, (messages) => {
             setMessages(messages);
         });
 
         // Listens for incoming messages
-         socketRef.current.off(NEW_CHAT_MESSAGE_EVENT, () => {
+        socketRef.current.off(NEW_CHAT_MESSAGE_EVENT, () => {
             setMessages();
         });
 
         // Listens for disconnection from server
-         socketRef.current.on('disconnect', () => { console.log('DISCONNECTED') });
+        socketRef.current.on('disconnect', () => { console.log('DISCONNECTED') });
     }
 
+    const disconnect = () => {
+        // Destroys the socket connection 
+        socketRef?.current?.disconnect(true);
+        // Destroys the socket reference when the connection is closed
+        socketRef?.current?.off('connection', () => { });
+    }
     return (
         <section className="chat" ref={messagesEndRef}>
             <MessageList data={messages} />
